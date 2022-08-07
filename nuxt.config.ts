b4a656956f98c04ea59673e6da5ff97ba94393c3
ansnekit <ansnekit@gmail.com>
2022-08-07 19:43:49 +0300

    fix configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,40 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: [  
-        "@nuxtjs/eslint-module",
+    css: ['~/assets/styles/global.scss'],
+    buildModules: [
+        '@nuxtjs/eslint-module',
 
-        ["@nuxtjs/eslint-module", { /* module options */ }],
+        [
+            '@nuxtjs/eslint-module',
+            {
+                /* module options */
+            },
+        ],
     ],
+    build: {
+        loaders: {
+            scss: {
+                implementation: require('sass'),
+            },
+        },
+    },
+    head: {
+        link: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.gstatic.com',
+                crossorigin: true,
+            },
+            {
+                rel: 'stylesheet',
+                href: 'fonts.googleapis.com/css2?family=Inter:wght@600&family=Source+Sans+Pro:wght@400;600&display=swap',
+            },
+        ],
+    },
 });
